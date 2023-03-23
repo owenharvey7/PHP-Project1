7a7829e3c7f47bb260837b4080af4f929117e3f8
Nathanael Sumpter <nsumpter98@gmail.com>
2023-03-22 23:51:39 -0400

    misc

--- Projects/Project 1/login.php
+++ Projects/Project 1/login.php
@@ -1,11 +1,11 @@
 <?php /** @noinspection ALL */
 
-$dsn = 'mysql:dbname=natsumpt;host=localhost';
+$dsn = 'mysql:dbname=oharvey;host=localhost';
 //DSN: The Data Source Name - it contains the information required to connect to the database.
 //dbname: use your database name on AMPPS/MAMPS or your user-name on Turing
 //host: use localhost on both AMPPS/MAMPS and Turing.
-$dbUser = 'nsumpter'; //use root on AMPPS/MAMPS or your user-name on Turing
-$dbPassword = 'Leanahtan1';
+$dbUser = 'oharvey'; //use root on AMPPS/MAMPS or your user-name on Turing
+$dbPassword = 'oharvey';
 //For password, use empty string on AMPPS/MAMPS or your password if you have one.
 //Default AMPPS/MAMPS password is mysql
 
